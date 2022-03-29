16c8599b706ef783a8c826d6e850898f345043d1
viren-vii <virubhosale112@gmail.com>
2022-03-30 05:12:48 +0530

    Added working forward function and IR code

--- main.py
+++ main.py
@@ -4,61 +4,58 @@ import pins
 import ir
 import md
 
+
 while True:
-    # md.forward(100,100)
-    print(ir.IRread())
+    IR = ir.IRread()
+    #forward
+    if(IR["IR1"] == 0 and IR["IR2"] == 0 and IR["IR3"] == 0 and IR["IR4"] == 1 and IR["IR5"] == 1 and IR["IR6"] == 0 and IR["IR7"] == 0 and IR["IR8"] == 0):
+        md.forward(100, 100)
 
-# while True:
-#     IR = ir.IRread()
-#     #forward
-#     if(IR["IR1"] == 0 and IR["IR2"] == 0 and IR["IR3"] == 0 and IR["IR4"] == 1 and IR["IR5"] == 1 and IR["IR6"] == 0 and IR["IR7"] == 0 and IR["IR8"] == 0):
-#         md.forward(100, 100)
+    if(IR["IR1"] == 0 and IR["IR2"] == 0 and IR["IR3"] == 0 and IR["IR4"] == 1 and IR["IR5"] == 0 and IR["IR6"] == 0 and IR["IR7"] == 0 and IR["IR8"] == 0):
+        md.forward(100, 100)
 
-#     if(IR["IR1"] == 0 and IR["IR2"] == 0 and IR["IR3"] == 0 and IR["IR4"] == 1 and IR["IR5"] == 0 and IR["IR6"] == 0 and IR["IR7"] == 0 and IR["IR8"] == 0):
-#         md.forward(100, 100)
+    if(IR["IR1"] == 0 and IR["IR2"] == 0 and IR["IR3"] == 1 and IR["IR4"] == 0 and IR["IR5"] == 0 and IR["IR6"] == 0 and IR["IR7"] == 0 and IR["IR8"] == 0):
+        md.forward(100, 100)
 
-#     if(IR["IR1"] == 0 and IR["IR2"] == 0 and IR["IR3"] == 1 and IR["IR4"] == 0 and IR["IR5"] == 0 and IR["IR6"] == 0 and IR["IR7"] == 0 and IR["IR8"] == 0):
-#         md.forward(100, 100)
+    #left
+    if(IR["IR1"] == 1 and IR["IR2"] == 1 and IR["IR3"] == 1 and IR["IR4"] == 1 and IR["IR5"] == 0 and IR["IR6"] == 0 and IR["IR7"] == 0 and IR["IR8"] == 0):
+        md.left(100, 100)
 
-#     #left
-#     if(IR["IR1"] == 1 and IR["IR2"] == 1 and IR["IR3"] == 1 and IR["IR4"] == 1 and IR["IR5"] == 0 and IR["IR6"] == 0 and IR["IR7"] == 0 and IR["IR8"] == 0):
-#         md.left(100, 100)
+    if(IR["IR1"] == 0 and IR["IR2"] == 1 and IR["IR3"] == 1 and IR["IR4"] == 1 and IR["IR5"] == 0 and IR["IR6"] == 0 and IR["IR7"] == 0 and IR["IR8"] == 0):
+        md.left(100, 100)
 
-#     if(IR["IR1"] == 0 and IR["IR2"] == 1 and IR["IR3"] == 1 and IR["IR4"] == 1 and IR["IR5"] == 0 and IR["IR6"] == 0 and IR["IR7"] == 0 and IR["IR8"] == 0):
-#         md.left(100, 100)
+    if(IR["IR1"] == 0 and IR["IR2"] == 0 and IR["IR3"] == 1 and IR["IR4"] == 1 and IR["IR5"] == 0 and IR["IR6"] == 0 and IR["IR7"] == 0 and IR["IR8"] == 0):
+        md.left(100, 100)
 
-#     if(IR["IR1"] == 0 and IR["IR2"] == 0 and IR["IR3"] == 1 and IR["IR4"] == 1 and IR["IR5"] == 0 and IR["IR6"] == 0 and IR["IR7"] == 0 and IR["IR8"] == 0):
-#         md.left(100, 100)
+    if(IR["IR1"] == 1 and IR["IR2"] == 0 and IR["IR3"] == 1 and IR["IR4"] == 1 and IR["IR5"] == 0 and IR["IR6"] == 0 and IR["IR7"] == 0 and IR["IR8"] == 0):
+        md.left(100, 100)
 
-#     if(IR["IR1"] == 1 and IR["IR2"] == 0 and IR["IR3"] == 1 and IR["IR4"] == 1 and IR["IR5"] == 0 and IR["IR6"] == 0 and IR["IR7"] == 0 and IR["IR8"] == 0):
-#         md.left(100, 100)
-
-#     if(IR["IR1"] == 1 and IR["IR2"] == 0 and IR["IR3"] == 0 and IR["IR4"] == 1 and IR["IR5"] == 0 and IR["IR6"] == 0 and IR["IR7"] == 0 and IR["IR8"] == 0):
-#         md.left(100, 100)
+    if(IR["IR1"] == 1 and IR["IR2"] == 0 and IR["IR3"] == 0 and IR["IR4"] == 1 and IR["IR5"] == 0 and IR["IR6"] == 0 and IR["IR7"] == 0 and IR["IR8"] == 0):
+        md.left(100, 100)
     
 
-#     #right
-#     if(IR["IR8"] == 1 and IR["IR7"] == 1 and IR["IR6"] == 1 and IR["IR5"] == 1 and IR["IR4"] == 0 and IR["IR3"] == 0 and IR["IR2"] == 0 and IR["IR1"] == 0):
-#         md.left(100, 100)
+    #right
+    if(IR["IR8"] == 1 and IR["IR7"] == 1 and IR["IR6"] == 1 and IR["IR5"] == 1 and IR["IR4"] == 0 and IR["IR3"] == 0 and IR["IR2"] == 0 and IR["IR1"] == 0):
+        md.left(100, 100)
 
-#     if(IR["IR8"] == 0 and IR["IR7"] == 1 and IR["IR6"] == 1 and IR["IR5"] == 1 and IR["IR4"] == 0 and IR["IR3"] == 0 and IR["IR2"] == 0 and IR["IR1"] == 0):
-#         md.left(100, 100)
+    if(IR["IR8"] == 0 and IR["IR7"] == 1 and IR["IR6"] == 1 and IR["IR5"] == 1 and IR["IR4"] == 0 and IR["IR3"] == 0 and IR["IR2"] == 0 and IR["IR1"] == 0):
+        md.left(100, 100)
 
-#     if(IR["IR8"] == 0 and IR["IR7"] == 0 and IR["IR6"] == 1 and IR["IR5"] == 1 and IR["IR4"] == 0 and IR["IR3"] == 0 and IR["IR2"] == 0 and IR["IR1"] == 0):
-#         md.left(100, 100)
+    if(IR["IR8"] == 0 and IR["IR7"] == 0 and IR["IR6"] == 1 and IR["IR5"] == 1 and IR["IR4"] == 0 and IR["IR3"] == 0 and IR["IR2"] == 0 and IR["IR1"] == 0):
+        md.left(100, 100)
 
-#     if(IR["IR8"] == 1 and IR["IR7"] == 0 and IR["IR6"] == 1 and IR["IR5"] == 1 and IR["IR4"] == 0 and IR["IR3"] == 0 and IR["IR2"] == 0 and IR["IR1"] == 0):
-#         md.left(100, 100)
+    if(IR["IR8"] == 1 and IR["IR7"] == 0 and IR["IR6"] == 1 and IR["IR5"] == 1 and IR["IR4"] == 0 and IR["IR3"] == 0 and IR["IR2"] == 0 and IR["IR1"] == 0):
+        md.left(100, 100)
 
-#     if(IR["IR8"] == 1 and IR["IR7"] == 0 and IR["IR6"] == 0 and IR["IR5"] == 1 and IR["IR4"] == 0 and IR["IR3"] == 0 and IR["IR2"] == 0 and IR["IR1"] == 0):
-#         md.left(100, 100)
+    if(IR["IR8"] == 1 and IR["IR7"] == 0 and IR["IR6"] == 0 and IR["IR5"] == 1 and IR["IR4"] == 0 and IR["IR3"] == 0 and IR["IR2"] == 0 and IR["IR1"] == 0):
+        md.left(100, 100)
     
-#     #stop
-#     if(IR["IR8"] == 0 and IR["IR7"] == 0 and IR["IR6"] == 0 and IR["IR5"] == 0 and IR["IR4"] == 0 and IR["IR3"] == 0 and IR["IR2"] == 0 and IR["IR1"] == 0):
-#         md.stop(100, 100)
+    #stop
+    if(IR["IR8"] == 0 and IR["IR7"] == 0 and IR["IR6"] == 0 and IR["IR5"] == 0 and IR["IR4"] == 0 and IR["IR3"] == 0 and IR["IR2"] == 0 and IR["IR1"] == 0):
+        md.stop(100, 100)
 
-#     #deg360
-#     if(IR["IR8"] == 1 and IR["IR7"] == 1 and IR["IR6"] == 1 and IR["IR5"] == 1 and IR["IR4"] == 1 and IR["IR3"] == 1 and IR["IR2"] == 1 and IR["IR1"] == 1):
-#         md.deg360(100, 100)
+    #deg360
+    if(IR["IR8"] == 1 and IR["IR7"] == 1 and IR["IR6"] == 1 and IR["IR5"] == 1 and IR["IR4"] == 1 and IR["IR3"] == 1 and IR["IR2"] == 1 and IR["IR1"] == 1):
+        md.deg360(100, 100)
     
     
